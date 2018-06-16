7d9087158a3d3c63a26e85dd3023317162be3304
Katharine Xu <39127532+KatharineX@users.noreply.github.com>
2018-06-16 23:12:42 +1000

    Update affirmation4u.py
    #adding daily

--- affirmation4u.py
+++ affirmation4u.py
@@ -2,16 +2,16 @@ import time
 ##Daily Affirmations
 
 
-user_input = input("Pop in a number between 1-7. See what the world has for you! " )
+user_input = input("Pop in a number between 1-8. See what the world has for you! " )
 answer = int(user_input)
 
 ##Dramatic effects 
 time.sleep(1)
 
 affirmations = ["The world has squat for you. Enjoy the vestiges of your youth.",
-"Baby, the stars are shining! Get a cup of coffee, listen to some sweet music and watch the stars tonight.", "You should definitely follow tsarstories on wordpress and tumblr!",
-"Read that book you've been putting off for ages.", "Smile! It looks good on you.", "You're gonna need a drink today.", "You! Are! Awesome!"]
+"Baby, the stars are shining! Get a cup of coffee, listen to some sweet music and watch the stars tonight.", "You should definitely follow tsarstories on wordpress and tumblr :)",
+"Read that book you've been putting off for ages.", "Smile! It looks good on you.", "You're gonna need a drink today.", "You! Are! Awesome!", "Don't cry because it's over. Smile because it happened."]
 
-##A happy customer
+##An affirmation for you!
 print("Your affirmation for the day is: {}".format(affirmations[answer-1]))
 
