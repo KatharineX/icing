e4a4bfa37228e3f48f7f123d61b79d09d999c53a
Katharine Xu <x.katharine71199@gmail.com>
2018-06-18 22:07:17 +1000

    Update affirmation4u.py

--- affirmation4u.py
+++ affirmation4u.py
@@ -2,14 +2,14 @@ import time
 ##Daily Affirmations
 
 
-user_input = input("Pop in a number between 1-9. See what the world has for you! " )
+user_input = input("Pop in a number between 1-10. See what the world has for you! " )
 answer = int(user_input)
 
 ##Dramatic effects 
 time.sleep(1)
 
-affirmations = ["The world has squat for you. Enjoy the vestiges of your youth.", "Study for your exams! Crush it!",
-"Baby, the stars are shining! Get a cup of coffee, listen to some sweet music and watch the stars tonight.", "You should definitely follow tsarstories on wordpress and tumblr :)",
+affirmations = ["The world has squat for you. Enjoy the vestiges of your youth.", "Study for your exams! Crush it!", "Try matcha! It has the benefits of caffiene without the negative side effects.",
+"Baby, the stars are shining! Get a cup of coffee, listen to some sweet music and watch the stars tonight.", "FEEL GOOD ABOUT UR DECISIONS. EVERYTHING HAS ITS REASON AND RHYME.",
 "Read that book you've been putting off for ages.", "Smile! It looks good on you.", "You're gonna need a drink today.", "You! Are! Awesome!", "Don't cry because it's over. Smile because it happened."]
 
 ##An affirmation for you!
